dcb90d2dd341745d021f75e8733ab57f38cd8d1e
resulfy <158181650+resulfy@users.noreply.github.com>
2024-12-16 11:34:12 +0300

    AppBar + App.vue güncellemeleri

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,31 @@
 <template>
   <v-app>
-    <Menu />
+    <v-main>
+      <div class="layout-container">
+      <Menu />
+      <div class="main-content">
+        <div class="app-bar">
+          <AppBar />
+        </div>
+        <div class="content">
+          <h1>Welcome to Narbulut</h1>
+          <p>Bu alana içerik eklenebilir.</p>
+        </div>
+      </div>
+    </div>
+    </v-main>
   </v-app>
 </template>
 
 <script>
 import Menu from "./components/Menu/Menu.vue";
+import AppBar from "./components/AppBar/AppBar.vue";
 
 export default {
   name: "App",
   components: {
     Menu,
+    AppBar,
   },
 };
 </script>
---
+++ src/components/AppBar/AppBar.vue
@@ -0,0 +1,47 @@
+<template>
+  <v-app>
+    <v-app-bar elevation="5" class="app-bar">
+      <v-text-field
+        prepend-inner-icon="mdi-magnify"
+        placeholder="Search"
+        variant="outlined"
+        hide-details
+        density="compact"
+        class="search-bar"
+      ></v-text-field>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon @click="toggleTheme">
+        <v-icon>{{ isDark ? 'mdi-weather-sunny' : 'mdi-weather-night' }}</v-icon>
+      </v-btn>
+
+      <v-avatar size="36" class="ml-3">
+        <img
+          src="https://randomuser.me/api/portraits/men/85.jpg"
+          alt="Avatar"
+        />
+      </v-avatar>
+    </v-app-bar>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: "CustomAppBar",
+  data() {
+    return {
+      isDark: false,
+    };
+  },
+  methods: {
+    toggleTheme() {
+      this.isDark = !this.isDark;
+      this.$vuetify.theme.global.name = this.isDark ? "dark" : "light";
+    },
+  },
+};
+</script>
+
+
+<style src="./AppBar.css"></style>
