a234240050a140a530ccb6a628445c97aac16452
resulfy <158181650+resulfy@users.noreply.github.com>
2024-12-18 09:28:44 +0300

    App overflow gone

--- src/components/UserTable/UserTable.vue
+++ src/components/UserTable/UserTable.vue
@@ -2,70 +2,20 @@
   <v-data-table
     :headers="headers"
     :items="users"
-    :sort-by="[{ key: 'name', order: 'asc' }]"
+    :loading="loadingUsers"
+    :sort-by="[{ key: 'id', order: 'asc' }]"
+    loading-text="Loading users... Please wait"
   >
     <template v-slot:top>
       <v-toolbar flat>
         <v-toolbar-title>Users Table</v-toolbar-title>
         <v-spacer></v-spacer>
-        <v-dialog v-model="dialog" max-width="500px">
-          <template v-slot:activator="{ props }">
-            <v-btn class="mb-2" color="primary" dark v-bind="props">
-              New User
-            </v-btn>
-          </template>
-          <v-card>
-            <v-card-title>
-              <span class="text-h5">{{ formTitle }}</span>
-            </v-card-title>
-
-            <v-card-text>
-              <v-container>
-                <v-row>
-                  <v-col cols="12">
-                    <v-text-field
-                      v-model="editedItem.name"
-                      label="Name"
-                    ></v-text-field>
-                  </v-col>
-                  <v-col cols="12">
-                    <v-text-field
-                      v-model="editedItem.email"
-                      label="E-mail"
-                    ></v-text-field>
-                  </v-col>
-                  <v-col cols="12">
-                    <v-text-field
-                      v-model="editedItem.address"
-                      label="Address"
-                    ></v-text-field>
-                  </v-col>
-                </v-row>
-              </v-container>
-            </v-card-text>
-
-            <v-card-actions>
-              <v-spacer></v-spacer>
-              <v-btn color="blue-darken-1" variant="text" @click="close">
-                Cancel
-              </v-btn>
-              <v-btn color="blue-darken-1" variant="text" @click="save">
-                Save
-              </v-btn>
-            </v-card-actions>
-          </v-card>
-        </v-dialog>
-        <v-dialog v-model="dialogDelete" max-width="500px">
-          <v-card>
-            <v-card-title class="text-h6">Are you sure you want to delete this item?</v-card-title>
-            <v-card-actions>
-              <v-spacer></v-spacer>
-              <v-btn color="blue-darken-1" variant="text" @click="closeDelete">Cancel</v-btn>
-              <v-btn color="blue-darken-1" variant="text" @click="deleteItemConfirm">OK</v-btn>
-              <v-spacer></v-spacer>
-            </v-card-actions>
-          </v-card>
-        </v-dialog>
+        <v-btn color="primary" dark @click="openNewUserDialog">
+          New User
+        </v-btn>
+        <v-btn color="primary" dark @click="fetchUsers">
+          Refresh Data
+        </v-btn>
       </v-toolbar>
     </template>
 
@@ -77,113 +27,181 @@
         mdi-delete
       </v-icon>
     </template>
-
-    <template v-slot:no-data>
-      <v-btn color="primary" @click="initialize">Reset</v-btn>
-    </template>
   </v-data-table>
+
+  <v-dialog v-model="dialog" max-width="500px">
+    <v-card>
+      <v-card-text>
+        <v-container>
+          <v-row>
+            <v-col cols="12">
+              <v-text-field v-model="editedItem.name" label="Name"></v-text-field>
+            </v-col>
+            <v-col cols="12">
+              <v-text-field v-model="editedItem.email" label="E-mail"></v-text-field>
+            </v-col>
+            <v-col cols="12">
+              <v-text-field v-model="editedItem.address" label="Address"></v-text-field>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-card-text>
+
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn color="blue-darken-1" variant="text" @click="close">
+          Cancel
+        </v-btn>
+        <v-btn color="blue-darken-1" variant="text" @click="save">
+          Save
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+
+  <v-dialog v-model="dialogDelete" max-width="500px">
+    <v-card>
+      <v-card-title class="text-h6">Are you sure you want to delete this item?</v-card-title>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn color="blue-darken-1" variant="text" @click="closeDelete">Cancel</v-btn>
+        <v-btn color="blue-darken-1" variant="text" @click="deleteItemConfirm">OK</v-btn>
+        <v-spacer></v-spacer>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
 </template>
 
 <script>
+import { ref, onMounted } from "vue";
+import axios from "axios";
+
 export default {
-  data: () => ({
-    dialog: false,
-    dialogDelete: false,
-    headers: [
+  name: "UserTable",
+  setup() {
+    const dialog = ref(false);
+    const dialogDelete = ref(false);
+    const users = ref([]);
+    const loadingUsers = ref(false);
+    const editedIndex = ref(-1);
+    const editedItem = ref({
+      name: "",
+      email: "",
+      address: "",
+    });
+    const defaultItem = {
+      name: "",
+      email: "",
+      address: "",
+    };
+
+    const headers = [
+      { title: "ID", key: "id", sortable: true },
       { title: "Name", key: "name" },
       { title: "E-mail", key: "email" },
       { title: "Address", key: "address" },
       { title: "Actions", key: "actions", sortable: false },
-    ],
-    users: [],
-    editedIndex: -1,
-    editedItem: {
-      name: "",
-      email: "",
-      address: "",
-    },
-    defaultItem: {
-      name: "",
-      email: "",
-      address: "",
-    },
-  }),
+    ];
 
-  computed: {
-    formTitle() {
-      return this.editedIndex === -1 ? "New User" : "Edit User";
-    },
-  },
-
-  watch: {
-    dialog(val) {
-      val || this.close();
-    },
-    dialogDelete(val) {
-      val || this.closeDelete();
-    },
-  },
-
-  created() {
-    this.initialize();
-  },
-
-  methods: {
-    initialize() {
-      this.users = [
-        {
-          name: "John Doe",
-          email: "john@example.com",
-          address: "123 Main St, Cityville",
-        },
-        {
-          name: "Jane Smith",
-          email: "jane@example.com",
-          address: "456 Elm St, Townsville",
-        },
-      ];
-    },
-
-    editItem(item) {
-      this.editedIndex = this.users.indexOf(item);
-      this.editedItem = Object.assign({}, item);
-      this.dialog = true;
-    },
-
-    deleteItem(item) {
-      this.editedIndex = this.users.indexOf(item);
-      this.dialogDelete = true;
-    },
-
-    deleteItemConfirm() {
-      this.users.splice(this.editedIndex, 1);
-      this.closeDelete();
-    },
-
-    close() {
-      this.dialog = false;
-      this.$nextTick(() => {
-        this.editedItem = Object.assign({}, this.defaultItem);
-        this.editedIndex = -1;
-      });
-    },
-
-    closeDelete() {
-      this.dialogDelete = false;
-      this.$nextTick(() => {
-        this.editedItem = Object.assign({}, this.defaultItem);
-        this.editedIndex = -1;
-      });
-    },
-
-    save() {
-      if (this.editedIndex > -1) {
-        Object.assign(this.users[this.editedIndex], this.editedItem);
-      } else {
-        this.users.push(this.editedItem);
+    const fetchUsers = async() => {
+      loadingUsers.value = true;
+      try {
+        const {data} = await axios.get("https://jsonplaceholder.typicode.com/users")
+        users.value = data.map((user) => ({
+            id: user.id,
+            name: user.name,
+            email: user.email,
+            address: `${user.address.street}, ${user.address.city}`,
+          }));
+          loadingUsers.value = false;
+      } catch (error) {
+        users.value= []
       }
-      this.close();
-    },
+      // axios
+      //   .get("https://jsonplaceholder.typicode.com/users")
+      //   .then((res) => {
+      //     users.value = res.data.map((user) => ({
+      //       id: user.id,
+      //       name: user.name,
+      //       email: user.email,
+      //       address: `${user.address.street}, ${user.address.city}`,
+      //     }));
+      //   })
+      //   .catch((error) => {
+      //     console.error("Error fetching users:", error);
+      //   })
+      //   .finally(() => {
+      //     loadingUsers.value = false;
+        // });
+    };
+
+    const openNewUserDialog = () => {
+      editedIndex.value = -1;
+      editedItem.value = Object.assign({}, defaultItem);
+      dialog.value = true;
+    };
+
+    const editItem = (item) => {
+      editedIndex.value = users.value.indexOf(item);
+      editedItem.value = Object.assign({}, item);
+      dialog.value = true;
+    };
+
+    const save = () => {
+      if (editedIndex.value > -1) {
+        Object.assign(users.value[editedIndex.value], editedItem.value);
+      } else {
+        users.value.push({
+          id: users.value.length + 1,
+          ...editedItem.value,
+        });
+      }
+      close();
+    };
+
+    const deleteItem = (item) => {
+      editedIndex.value = users.value.indexOf(item);
+      dialogDelete.value = true;
+    };
+
+    const deleteItemConfirm = () => {
+      if (editedIndex.value > -1) {
+        users.value.splice(editedIndex.value, 1);
+      }
+      closeDelete();
+    };
+
+    const close = () => {
+      dialog.value = false;
+      editedIndex.value = -1;
+    };
+
+    const closeDelete = () => {
+      dialogDelete.value = false;
+      editedIndex.value = -1;
+    };
+
+    onMounted(() => {
+       fetchUsers();
+    });
+
+    return {
+      dialog,
+      dialogDelete,
+      users,
+      loadingUsers,
+      editedItem,
+      editedIndex,
+      headers,
+      fetchUsers,
+      openNewUserDialog,
+      editItem,
+      save,
+      deleteItem,
+      deleteItemConfirm,
+      close,
+      closeDelete,
+    };
   },
 };
 </script>
